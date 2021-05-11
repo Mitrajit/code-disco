8b4e04c30cdb9db0b650ca82093d5c584f875cee
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-05-11 15:13:48 +0530

    Lapping feature added and stopwatch mistake corrected

--- web/js/script.js
+++ web/js/script.js
@@ -4,7 +4,8 @@ var interval;
 let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 function Time() {
     clearInterval(interval);
-    document.getElementById("Stopwatch").classList.add("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[0].classList.add("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[1].classList.add("visually-hidden");
     document.getElementById("Time").classList.remove("visually-hidden");
     interval = setInterval(() => {
         let date = new Date;
@@ -25,25 +26,46 @@ function Time() {
 function StopWatch() {
     clearInterval(interval);
     document.getElementById("Time").classList.add("visually-hidden");
-    document.getElementById("Stopwatch").classList.remove("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[0].classList.remove("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[1].classList.remove("visually-hidden");
 }
-let timing = 0;
+let timing = 0,prevlap=0, lapind=1;
 function initiatestopwatch(action) {
     if (action == "start") {
         interval = setInterval(() => {
-            document.getElementById("stopwatch_time").innerText = Math.floor(timing / (10 * 60 * 60)).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + ":" + (Math.floor(timing / (10 * 60))%60).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + ":" + (Math.floor(timing / (10))%60).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + "." + (timing%10).toLocaleString("en-US", { minimumIntegerDigits: 2 });
+            document.getElementById("stopwatch_time").innerText = formattime(timing);
             timing += 1;
         }, 100);
         document.getElementById("initiatestopwatch").innerText = "Stop";
+        document.getElementById("lap_or_reset").innerText = "Lap";
     }
     else {
         clearInterval(interval);
         document.getElementById("initiatestopwatch").innerText = "Start";
+        document.getElementById("lap_or_reset").innerText = "Reset";
     }
 }
-function resetstopwatch() {
-    clearInterval(interval);
-    timing=0;
-    document.getElementById('stopwatch_time').innerText = '00:00:00.00';
-    document.getElementById("initiatestopwatch").innerText = "Start";
+function formattime(time){
+    return Math.floor(time / (10 * 60 * 60)).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + ":" + (Math.floor(time / (10 * 60)) % 60).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + ":" + (Math.floor(time / (10)) % 60).toLocaleString("en-US", { minimumIntegerDigits: 2 }) + "." + (time % 10);
+}
+function lap_or_resetstopwatch(action) {
+    if (action=="Reset"){
+        clearInterval(interval);
+        timing = 0;
+        prevlap = 0;
+        lapind=1
+        document.getElementById('stopwatch_time').innerText = '00:00:00.0';
+        document.getElementById("initiatestopwatch").innerText = "Start";
+        document.getElementById("laps").innerHTML="";
+    }
+    else{
+        let laptime=timing-prevlap;
+        prevlap = timing
+        document.getElementById("laps").innerHTML+=`
+        <tr>
+            <th scope="row">${lapind++}</th>
+            <td>${formattime(laptime)}</td>
+            <td>${formattime(timing)}</td>
+          </tr>`
+    }
 }
