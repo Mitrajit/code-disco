66b881a4af4879395c3b97ed0590fce40540cfc1
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-05-12 20:43:20 +0530

    Alarm has been added and minor optimization done

--- web/js/script.js
+++ web/js/script.js
@@ -1,28 +1,70 @@
 console.log("Hello World");
-Time();
 var interval;
 let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-let offset = moment(new Date()).format("ZZ");
+let offset = moment().format("ZZ");
+var time;
 timezone+=" (UTC"+offset.slice(0,3)+":"+offset.slice(3)+")";
+Time();
 function Time() {
     clearInterval(interval);
     document.getElementsByClassName("Stopwatch")[0].classList.add("visually-hidden");
     document.getElementsByClassName("Stopwatch")[1].classList.add("visually-hidden");
+    document.getElementById("Alarm").classList.add("visually-hidden");
     document.getElementById("Time").classList.remove("visually-hidden");
-    interval = setInterval(() => {
-        let date = new Date;
-        let Utc = moment.utc(date).utcOffset(date.getTimezoneOffset).format('YYYY/MM/DD HH:mm:ss ZZ');
-        moment.utc(Utc).utcOffset(offset).format('dddd, MMM DD,YYYY HH:mm:ss A')
-        document.getElementById("timeanddate").innerHTML = moment.utc(Utc).utcOffset(offset).format('hh:mm:ss A')
-         + "<br><h1 class=\"display-6 align-text-top\">" + moment.utc(Utc).utcOffset(offset).format('dddd, MMM DD,YYYY')
-         + "</h1><h5>" + timezone + "</h5>"
-    }, 1000);
+    tick();
+    interval = setInterval(tick, 1000);
+}
+function tick(){
+    time = moment.utc().utcOffset(offset);
+    document.getElementById("timeanddate").innerHTML = moment.utc().utcOffset(offset).format('hh:mm:ss A')
+     + "<br><h1 class=\"display-6 align-text-top\">" + moment.utc().utcOffset(offset).format('dddd, MMM DD,YYYY')
+     + "</h1><h5>" + timezone + "</h5>"
+    document.getElementById("Alarm").getElementsByTagName("h4")[0].innerText = "Current time "+moment.utc().utcOffset(offset).format('hh:mm:ss A');
+    if(document.getElementById("initiatealarm").innerText=="Reset"){
+        let thespan = document.getElementById("alarm_time");
+        let alarmtime = moment(thespan.getElementsByTagName("input")[0].value,"HH:mm");
+        let T=moment.utc(moment(alarmtime,"HH:mm").diff(moment())+1000).format(" (T-HH:mm:ss)");
+        thespan.getElementsByTagName("time")[0].innerText = alarmtime.format("hh:mm A ")+T;
+        if(T==" (T-00:00:00)"){
+            initiatealarm("reset");
+            alert("Alarm !!!!");
+        }
+    }
 }
 function StopWatch() {
     clearInterval(interval);
     document.getElementById("Time").classList.add("visually-hidden");
+    document.getElementById("Alarm").classList.add("visually-hidden");
     document.getElementsByClassName("Stopwatch")[0].classList.remove("visually-hidden");
     document.getElementsByClassName("Stopwatch")[1].classList.remove("visually-hidden");
+}
+function Alarm() {
+    document.getElementById("Time").classList.add("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[0].classList.add("visually-hidden");
+    document.getElementsByClassName("Stopwatch")[1].classList.add("visually-hidden");
+    document.getElementById("Alarm").classList.remove("visually-hidden");
+}
+function initiatealarm(action) {
+    let thespan = document.getElementById("alarm_time");
+    if (action=="set"){
+        if(thespan.getElementsByTagName("input")[0].value == "")
+            return;
+        thespan.getElementsByTagName("input")[0].classList.add("visually-hidden");
+        thespan.getElementsByTagName("time")[0].classList.remove("visually-hidden");
+        document.getElementById("initiatealarm").innerText = "Reset";
+        tick();
+    }
+    else
+    {
+        thespan.getElementsByTagName("input")[0].classList.remove("visually-hidden");
+        thespan.getElementsByTagName("time")[0].classList.add("visually-hidden");
+        document.getElementById("initiatealarm").innerText = "Set";
+    }
+
+}
+function inputalarm() {
+
+
 }
 let timing = 0,prevlap=0, lapind=1;
 function initiatestopwatch(action) {
